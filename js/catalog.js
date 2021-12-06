5a323d27847bec5b42134847ae031426fec6121b
Core2-0 <Core2-00@mail.ru>
2021-12-06 08:00:52 +0500

    Update catalog
    Добавлена загрузка карточек товара из json

---
+++ js/catalog.js
@@ -0,0 +1,55 @@
+let requestURL = 'server/catalog.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = () => {
+  const products = request.response;
+  createCard(products);
+  renderCard(products);
+};
+
+const makeElement = (tagName, className, text) => {
+  let element = document.createElement(tagName);
+  if (className != '') {
+    element.classList.add(className);
+  }
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+};
+
+const createCard = (products) => {
+  let listItem = makeElement('li', 'products-item');
+
+  let description = makeElement('div', 'products-description');
+  listItem.appendChild(description);
+
+  let title = makeElement('a', 'products-link', products.title);
+  title.href = '#';
+  description.appendChild(title);
+
+  let text = makeElement('p', '', products.description);
+  description.appendChild(text);
+
+  let priceButton = makeElement('a', 'button', products.price + ' ₽');
+  priceButton.classList.add('products-item-button');
+  description.appendChild(priceButton);
+
+  let picture = makeElement('img', '');
+  picture.src = products.imgUrl;
+  listItem.appendChild(picture);
+
+  return listItem;
+};
+
+const renderCard = (products) => {
+  let cardList = document.querySelector('.products-list');
+
+  for (let i = 0; i < products.length; i += 1) {
+    let cardItem = createCard(products[i]);
+    cardList.appendChild(cardItem);
+  }
+};
