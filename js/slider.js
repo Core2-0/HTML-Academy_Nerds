858180079952da59c76a360d5fd636f083a6d89c
Core2-0 <Core2-00@mail.ru>
2021-12-07 07:26:32 +0500

    update Slider
    Необходимо поправить фон

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,13 @@
-const sliderButtonOne = document.querySelector(".slider-controls button");
-const sliderButtonTwo = document.querySelector(".slider-controls button:nth-child(2)");
-const sliderButtonThree = document.querySelector(".slider-controls button:nth-child(3)");
+const sliderControls = document.querySelector('.slider-controls');
 
-const sliderItemOne = document.querySelector(".slider-item");
-const sliderItemTwo = document.querySelector(".slider-item:nth-child(2)");
-const sliderItemThree = document.querySelector(".slider-item:nth-child(3)");
+sliderControls.addEventListener('click', function (evt) {
+  const target = evt.target;
+  if (target.classList.contains('slider-control') && !target.classList.contains('current')) {
+    const slideId = target.dataset.slide;
 
-const sliderBackground = document.querySelector(".slider-list");
-
-
-sliderButtonOne.addEventListener("click", function () {
-  this.classList.add("current");
-  sliderItemOne.classList.add("slide-current");
-  sliderBackground.classList.add("slider-background-1");
-
-  sliderButtonTwo.classList.remove("current");
-  sliderButtonThree.classList.remove("current");
-  sliderItemTwo.classList.remove("slide-current");
-  sliderItemThree.classList.remove("slide-current");
-  sliderBackground.classList.remove("slider-background-2");
-  sliderBackground.classList.remove("slider-background-3");
-});
-
-sliderButtonTwo.addEventListener("click", function () {
-  this.classList.add("current");
-  sliderItemTwo.classList.add("slide-current");
-  sliderBackground.classList.add("slider-background-2");
-
-  sliderButtonOne.classList.remove("current");
-  sliderButtonThree.classList.remove("current");
-  sliderItemOne.classList.remove("slide-current");
-  sliderItemThree.classList.remove("slide-current");
-  sliderBackground.classList.remove("slider-background-1");
-  sliderBackground.classList.remove("slider-background-3");
-});
-
-sliderButtonThree.addEventListener("click", function () {
-  this.classList.add("current");
-  sliderItemThree.classList.add("slide-current");
-  sliderBackground.classList.add("slider-background-3");
-
-  sliderButtonOne.classList.remove("current");
-  sliderButtonTwo.classList.remove("current");
-  sliderItemOne.classList.remove("slide-current");
-  sliderItemTwo.classList.remove("slide-current");
-  sliderBackground.classList.remove("slider-background-1");
-  sliderBackground.classList.remove("slider-background-2");
+    document.querySelector('.slider-control.current').classList.remove('current');
+    target.classList.add('current');
+    document.querySelector('.slider-item.slide-current').classList.remove('slide-current');
+    document.querySelector(`[data-id="${slideId}"]`).classList.add('slide-current');
+  }
 });
